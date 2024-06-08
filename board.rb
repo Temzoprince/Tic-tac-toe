7c364fb7d5e9ce6bca808beacc155b62f1af5226
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-06-08 02:10:25 +0100

    All methods for board works

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Board
   def initialize
     @grid = [['', '', ''],
@@ -6,12 +8,16 @@ class Board
   end
 
   def populate_table(row, column, shape)
-    @grid[row - 1][column - 1] = shape if @grid[row - 1][column - 1].empty?
+    if @grid[row - 1][column - 1].empty?
+      @grid[row - 1][column - 1] = shape
+    else
+      puts 'That spot has already been filled. Go again'
+    end
   end
 
   def clear_table
     @grid.each do |row|
-      row.map! { |value| value = '' }
+      row.map! { |_value| '' }
     end
   end
 
@@ -21,3 +27,21 @@ class Board
     p @grid[2]
   end
 end
+
+board = Board.new
+board.display_table
+board.populate_table(2, 2, 'X')
+puts ''
+board.display_table
+board.populate_table(2, 2, 'O')
+puts ''
+board.display_table
+board.populate_table(3, 2, 'X')
+puts ''
+board.display_table
+board.populate_table(1, 2, 'X')
+puts ''
+board.display_table
+board.clear_table
+puts ''
+board.display_table
