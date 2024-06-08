68f7af10ea8109923aee3967f7242a60d6c02a4d
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-06-08 02:00:24 +0100

    Add methods for board

--- board.rb
+++ board.rb
@@ -4,4 +4,20 @@ class Board
              ['', '', ''],
              ['', '', '']]
   end
+
+  def populate_table(row, column, shape)
+    @grid[row - 1][column - 1] = shape if @grid[row - 1][column - 1].empty?
+  end
+
+  def clear_table
+    @grid.each do |row|
+      row.map! { |value| value = '' }
+    end
+  end
+
+  def display_table
+    p @grid[0]
+    p @grid[1]
+    p @grid[2]
+  end
 end
